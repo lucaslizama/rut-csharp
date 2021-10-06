3491e3523ec74883b56d38124ec092a051828ce2
Lucas Lizama <lucaslizama4@gmail.com>
2021-10-06 00:05:51 -0300

    Removed innesesary constructors from Rut class

--- Rut/Rut.cs
+++ Rut/Rut.cs
@@ -10,10 +10,15 @@ namespace Rut
 
         public int Number { get => number; set => number = value; }
         public char Dv { get => dv; set => dv = value; }
-        public string RutWithDots { get => Formatter.FormatWithDots(number, dv); }
-        public string RutWithDotsNoDv { get => Formatter.FormatWithDotsNoDv(number); }
+        public string WithDots { get => Formatter.FormatWithDots(number, dv); }
+        public string WithDotsNoDv { get => Formatter.FormatWithDotsNoDv(number); }
+        public string CleanRut { get => Formatter.FormatClean(number, dv); }
         public bool Valid { get => Validator.IsValid(number, dv); }
 
+        /// <summary>
+        /// Creates a rut object from a rut number and parse
+        /// </summary>
+        /// <param name="rut"></param>
         public Rut(int rut)
         {
             if (!Validator.IsValid(rut)) throw new InvalidRutInputException("The rut is invalid!");
@@ -21,25 +26,33 @@ namespace Rut
             this.dv = Calculator.CalculateDV(rut);
         }
 
+        /// <summary>
+        /// Creates a rut object from the rut number and dv
+        /// received, even if they are not valid.
+        /// </summary>
+        /// <param name="rut">Rut number</param>
+        /// <param name="dv">Rut Dv</param>
         public Rut(int rut, char dv)
         {
-            if (!Validator.IsValid(rut, dv)) throw new InvalidRutInputException("The rut is invalid!");
             this.number = rut;
             this.dv = dv;
         }
 
+        /// <summary>
+        /// Parses rut from a string in a variety of formats.
+        /// 
+        /// Supported formats: 
+        /// 1. 11.111.111-1
+        /// 2. 11111111-1
+        /// 3. 11111111
+        /// 
+        /// </summary>
+        /// <param name="rut">Valid rut string</param>
         public Rut(string rut)
         {
             if (!Validator.IsValid(rut)) throw new InvalidRutInputException("The rut is invalid!");
             this.number = int.Parse(rut);
             this.dv = Calculator.CalculateDV(rut);
-        }
-
-        public Rut(string rut, char dv)
-        {
-            if (!Validator.IsValid(rut, dv)) throw new InvalidRutInputException("The rut is invalid!");
-            this.number = int.Parse(rut);
-            this.dv = dv;
         }
     }
 }
