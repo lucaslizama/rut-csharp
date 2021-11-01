82bb8b16959f42c2c28e41c0e7133d5a23ac6f0c
Lucas Lizama <lucas@devaid.cl>
2021-11-01 20:10:14 -0300

    Added more tests for Generator class

--- Rut.Tests/UtilsTests/GeneratorTests.cs
+++ Rut.Tests/UtilsTests/GeneratorTests.cs
@@ -1,5 +1,7 @@
-using Xunit;
+using System.Collections.Generic;
+using System.Linq;
 using Rut.Utils;
+using Xunit;
 
 namespace Rut.Tests.UtilsTests
 {
@@ -8,8 +10,47 @@ namespace Rut.Tests.UtilsTests
         [Fact]
         public void ShouldGenerateValidRandomRut()
         {
-            Rut rut = Generator.GenerateRandomRut();
-            Assert.True(rut.Valid);
+            var ruts = new List<Rut>();
+            for (int i = 0; i < 100; i++)
+            {
+                ruts.Add(Generator.GenerateRandomRut());
+            }
+            Assert.True(ruts.All(rut => rut.IsValid));
+        }
+
+        [Theory]
+        [InlineData('0')]
+        [InlineData('1')]
+        [InlineData('2')]
+        [InlineData('3')]
+        [InlineData('4')]
+        [InlineData('5')]
+        [InlineData('6')]
+        [InlineData('7')]
+        [InlineData('8')]
+        [InlineData('9')]
+        [InlineData('K')]
+        public void ShouldGenerateValidRandomRutWithSpecifiedDv(char dv)
+        {
+            var ruts = new List<Rut>();
+            for (int i = 0; i < 100; i++)
+            {
+                ruts.Add(Generator.GenerateRandomRut(dv));
+            }
+            Assert.True(ruts.All(rut => rut.Dv == dv));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(50)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void ShouldGenerateAListOfRandomRutsOfACertainLength(int length)
+        {
+            var ruts = Generator.GenerateRandomRuts(length);
+            Assert.Equal(length, ruts.Count);
         }
     }
 }
